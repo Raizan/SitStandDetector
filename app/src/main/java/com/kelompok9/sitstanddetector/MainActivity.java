04f8dc39419417fd62250402b6f25deadfecf2ea
Raizan <reisuke.raizan@gmail.com>
2015-11-13 00:29:25 +0700

    Added Naive Bayes Classifier library. Added detection feature.

--- app/src/main/java/com/kelompok9/sitstanddetector/MainActivity.java
+++ app/src/main/java/com/kelompok9/sitstanddetector/MainActivity.java
@@ -21,7 +21,9 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -31,8 +33,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+
+import de.daslaboratorium.machinelearning.BayesClassifier;
+import de.daslaboratorium.machinelearning.Classification;
+import de.daslaboratorium.machinelearning.Classifier;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -46,12 +56,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView xText;
     private TextView yText;
     private TextView zText;
+    private TextView statusText;
 
     boolean isRecording;
     int globalDataCounter = 0;
+    int resettableDataCounter = 0;
 
     File sdCard;
     File directory;
+
+    Classifier<String, String> bayes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,23 +87,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // Sensor
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        // Register sensor
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
 
         // x y z generated display
         xText = (TextView) findViewById(R.id.xText);
         yText = (TextView) findViewById(R.id.yText);
         zText = (TextView) findViewById(R.id.zText);
 
-//        TextView sensorListTV = (TextView) findViewById(R.id.sensorListTV);
-//        sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-//        for (Sensor sensor : sensorList) {
-//            sensorListTV.append(sensor.getName());
-//        }
+        statusText = (TextView) findViewById(R.id.statusText);
 
         // Path to SDCard
         sdCard = Environment.getExternalStorageDirectory();
         directory = new File (sdCard.getAbsolutePath());
 
         recordedData = new ArrayList<AccelerometerData>();
+
+        // Naive bayes
+        bayes = new BayesClassifier<String, String>();
+        // Forget learned data after reaching 500 data milestone
+        // bayes.setMemoryCapacity(500);
+        // Initial state
+        state = "STOP";
     }
 
     public void onToggleBtnClicked(View v) throws IOException {
@@ -99,18 +119,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             state = "START";
             toggleBtn.setText("Stop");
             writeToFile("dataset.txt", "START\n");
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+//            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
         }
         else {
             state = "STOP";
             toggleBtn.setText("Start");
-            mSensorManager.unregisterListener(this);
+//            mSensorManager.unregisterListener(this);
         }
     }
 
     public void onBerdiriBtnClicked(View v) throws IOException {
         TextView berdiriBtn = (TextView) findViewById(R.id.berdiriBtn);
-        if (state == "STOP") {
+        if (state.equals("STOP")) {
             Toast toast = Toast.makeText(getApplicationContext(), "BERDIRI", Toast.LENGTH_SHORT);
             toast.show();
             writeToFile("dataset.txt", "BERDIRI\n");
@@ -119,16 +139,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void onDudukBtnClicked(View v) throws IOException {
         TextView dudukBtn = (TextView) findViewById(R.id.dudukBtn);
-        if (state == "STOP") {
+        if (state.equals("STOP")) {
             Toast toast = Toast.makeText(getApplicationContext(), "DUDUK", Toast.LENGTH_SHORT);
             toast.show();
             writeToFile("dataset.txt", "DUDUK\n");
         }
     }
 
-    public void onReadBtnClicked(View v){
-        TextView readBtn = (TextView) findViewById(R.id.readBtn);
+    public void onLabelBtnClicked(View v) throws IOException {
+        TextView labelBtn = (TextView) findViewById(R.id.labelBtn);
+        Toast toast = Toast.makeText(getApplicationContext(), "labelledDataSet.txt updated", Toast.LENGTH_SHORT);
+        toast.show();
         labelByUser();
+    }
+
+    public void onDetectBtnClicked(View v) throws IOException {
+        TextView detectBtn = (TextView) findViewById(R.id.detectBtn);
+        state = "DETECT";
+    }
+
+    public void onDetectObsBtnClicked(View v) throws IOException {
+        TextView detectObsBtn = (TextView) findViewById(R.id.detectObsBtn);
+        state = "DETECT-OBS";
+    }
+
+    public void onResetBtnClicked(View v) throws IOException {
+        TextView resetBtn = (TextView) findViewById(R.id.resetBtn);
+        reset();
     }
 
     @Override
@@ -166,26 +203,63 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         yText.setText("y:\t" + y);
         zText.setText("z:\t" + z);
 
-        AccelerometerData setData = new AccelerometerData(x, y, z);
-        recordedData.add(setData);
-
-        if (globalDataCounter % 10 == 0) {
-            float xSum = 0;
-            float ySum = 0;
-            float zSum = 0;
-            int quantity = 0;
-            for (AccelerometerData data : recordedData) {
-                xSum += data.x;
-                ySum += data.y;
-                zSum += data.z;
-                quantity += 1;
+        if (resettableDataCounter == 20)
+        {
+            float xSumAvg = 0;
+            float ySumAvg = 0;
+            float zSumAvg = 0;
+            // calculate avg
+            for (AccelerometerData dt : recordedData) {
+                xSumAvg += dt.x;
+                ySumAvg += dt.y;
+                zSumAvg += dt.z;
             }
-            float xAvg = xSum / (float) quantity;
-            float yAvg = ySum / (float) quantity;
-            float zAvg = zSum / (float) quantity;
 
+            float xAvg = xSumAvg / (float) recordedData.size();
+            float yAvg = ySumAvg / (float) recordedData.size();
+            float zAvg = zSumAvg / (float) recordedData.size();
 
-            String prepare = xAvg + " " + yAvg + " " + zAvg + "\n";
+            // analyze and display on TextView Status
+            String avgNormalized = normalize(xAvg, yAvg, zAvg);
+            String category = "";
+            if (state.equals("DETECT")) {
+                category = detect(avgNormalized);
+            } else if (state.equals("DETECT-OBS")) {
+                String arrayString[] = avgNormalized.split("\\s");
+                if (Float.valueOf(arrayString[1]) == 1.0) {
+                    category = "BERDIRI";
+                }
+                else if (Float.valueOf(arrayString[2]) == 1.0) {
+                    category = "DUDUK";
+                }
+            }
+
+            Toast toast = Toast.makeText(getApplicationContext(), "Status: " + category, Toast.LENGTH_SHORT);
+            toast.show();
+            statusText.setText("Status: " + category);
+
+            String prepare = avgNormalized + " " + category;
+            try {
+                writeToFile("labelledDataSet.txt", prepare);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // reset state
+            recordedData.clear();
+            resettableDataCounter = 0;
+            state = "STOP";
+        }
+
+        if (state.equals("DETECT") || state.equals("DETECT-OBS")) {
+            AccelerometerData recordData = new AccelerometerData(x, y, z);
+            recordedData.add(recordData);
+
+            resettableDataCounter += 1;
+        }
+        else if (state.equals("START")){
+            // Write RAW accelerometer data to file
+            String prepare = x + " " + y + " " + z + "\n";
             try {
                 writeToFile("dataset.txt", prepare);
             } catch (IOException e) {
@@ -225,6 +299,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         osw.close();
     }
 
+
+    private String normalize(float xAvg, float yAvg, float zAvg) {
+        float maxValue = Math.max(Math.max(Math.abs(xAvg), Math.abs(yAvg)), Math.abs(zAvg));
+        float xAvgNormal = Math.abs(xAvg) / maxValue;
+        float yAvgNormal = Math.abs(yAvg) / maxValue;
+        float zAvgNormal = Math.abs(zAvg) / maxValue;
+
+        return xAvgNormal + " " + yAvgNormal + " " + zAvgNormal;
+    }
+
     private void labelByUser() {
         //Get the text file
         File file = new File(directory, "dataset.txt");
@@ -236,10 +320,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
 
-            float xSum = 0;
-            float ySum = 0;
-            float zSum = 0;
+            float x = 0;
+            float y = 0;
+            float z = 0;
             int quantity = 0;
+
+            ArrayList<AccelerometerData> avgData = new ArrayList<AccelerometerData>();
+
             while ((line = br.readLine()) != null) {
                 String arrayString[] = line.split("\\s+");
                 if (arrayString[0].equals("START")) {
@@ -248,18 +335,79 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     continue;
                 } else if (arrayString[0].equals("BERDIRI") || arrayString[0].equals("DUDUK")) {
                     // calculate average
-                    // writeToFile("labelledDataSet.txt", xyzSum + " " + arrayString[0])
+                    float xAvg = x / (float) quantity;
+                    float yAvg = y / (float) quantity;
+                    float zAvg = z / (float) quantity;
+                    quantity = 0;
+                    x = 0;
+                    y = 0;
+                    z = 0;
+                    String avgNormalized = normalize(xAvg, yAvg, zAvg);
+                    String prepare = avgNormalized + " " + arrayString[0] + "\n";
+
+                    writeToFile("labelledDataSet.txt", prepare);
                 } else {
-                    xSum += Float.valueOf(arrayString[0]);
-                    ySum += Float.valueOf(arrayString[1]);
-                    zSum += Float.valueOf(arrayString[2]);
+                    x += Float.valueOf(arrayString[0]);
+                    y += Float.valueOf(arrayString[1]);
+                    z += Float.valueOf(arrayString[2]);
+                    quantity += 1;
                 }
-                quantity += 1;
             }
             br.close();
         }
         catch (IOException e) {
             //You'll need to add proper error handling here
         }
+    }
+
+    private void reset() {
+        File file = new File(directory, "labelledDataSet.txt");
+
+        //Read text from file
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String arrayString[] = line.split("\\s+");
+                if (arrayString[0].equals("NaN")) {
+                    continue;
+                } else {
+                    String categorized = categorize(arrayString);
+                    bayes.learn(arrayString[3], Arrays.asList(categorized.split("\\s")));
+                }
+            }
+            Toast toast = Toast.makeText(getApplicationContext(), "Bayes re-learned", Toast.LENGTH_SHORT);
+            toast.show();
+            br.close();
+        }
+        catch (IOException e) {
+            //You'll need to add proper error handling here
+        }
+    }
+
+    private String categorize(String[] arrayString) {
+        // hi low
+        StringBuilder categorized = new StringBuilder();
+        float threshold = (float) 0.4;
+        if (Float.valueOf(arrayString[0]) >= threshold) {
+            categorized.append("x ");
+
+        }
+        if (Float.valueOf(arrayString[1]) >= threshold) {
+            categorized.append("y ");
+        }
+        if (Float.valueOf(arrayString[2]) >= threshold) {
+            categorized.append(" z");
+        }
+        Toast toast = Toast.makeText(getApplicationContext(), categorized.toString(), Toast.LENGTH_SHORT);
+        toast.show();
+
+        return categorized.toString();
+    }
+
+    private String detect(String avgNormalized) {
+        return bayes.classify(Arrays.asList(avgNormalized.split("\\s"))).getCategory();
     }
 }
